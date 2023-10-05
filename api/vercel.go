7428b256bf10e7c1483ef0dc5c63237a8f1d6e15
GenshinMinecraft <68975944+GenshinMinecraft@users.noreply.github.com>
2023-10-05 13:55:19 +0800

    Update vercel.go

--- api/vercel.go
+++ api/vercel.go
@@ -12,7 +12,6 @@ import (
 func Vercel(w http.ResponseWriter, r *http.Request) {
 	conf.BotToken = os.Getenv("token")
 	conf.ChannelName = os.Getenv("channel")
-	conf.Pass = os.Getenv("pass")
 	// 获取请求路径
 	path := r.URL.Path
 	// 如果请求路径以 "/img/" 开头
@@ -29,10 +28,8 @@ func Vercel(w http.ResponseWriter, r *http.Request) {
 	switch path {
 	case "/api":
 		// 调用 control 包中的 UploadImageAPI 处理函数
-		control.Middleware(control.UploadImageAPI)(w, r)
-	case "/pwd":
-		control.Pwd(w, r)
+		control.UploadImageAPI(w, r)
 	default:
-		control.Middleware(control.Index)(w, r)
+		control.Index(w, r)
 	}
 }
